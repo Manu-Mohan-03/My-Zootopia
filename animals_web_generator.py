0313fb9f040a69f933c7b2a5fa83feec210ec775
Manu Mohan <manu.mohan0303@gmail.com>
2025-09-01 09:26:04 +0200

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,20 +1,41 @@
 import json
 
 def load_data(file_path):
-  """ Loads a JSON file """
-  with open(file_path, "r") as handle:
-    return json.load(handle)
+    """ Loads a JSON file """
+    with open(file_path, "r") as handle:
+        return json.load(handle)
 
-def print_details(fox_data):
+
+def get_animal_info(fox_data):
+    animal_info_str = ""
     for fox in fox_data:
-        print("Name: ", fox['name'])
-        print("Diet: ", fox["characteristics"]["diet"])
-        print("Location: ", fox["locations"][0])
+        animal_info_str += "Name: " + fox['name'] + "\n"
+        if fox.get("characteristics").get("diet"):
+            animal_info_str += "Diet: " + fox["characteristics"]["diet"] + "\n"
+        if fox.get("locations") and len(fox.get("locations")) > 0:
+            animal_info_str += "Location: " + fox["locations"][0]  + "\n"
         if fox.get("characteristics").get("type"):
-            print("Type: ", fox["characteristics"]["type"])
-        print()
+            animal_info_str += "Type: " + fox["characteristics"]["type"]  + "\n"
+        animal_info_str += "\n"
+    return animal_info_str
+
+def edit_html_template(file_path,animals_info):
+    """Open the html file for reading"""
+    with open(file_path,"r", encoding="utf-8") as handle:
+        html_template = handle.read()
+    html_template = html_template.replace("__REPLACE_ANIMALS_INFO__", animals_info)
+    return html_template
+
+def write_animals_html(file_path,animals_html):
+    with open(file_path, "w", encoding = "utf-8") as handle:
+        handle.write(animals_html)
 
 
-animals_data = load_data('animals_data.json')
+def main():
+    animals_data = load_data('animals_data.json')
+    animals_info = get_animal_info(animals_data)
+    animals_html = edit_html_template("animals_template.html",animals_info)
+    write_animals_html("animals.html", animals_html)
 
-print_details(animals_data)
+if __name__ == "__main__":
+    main()
